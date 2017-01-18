32ccec546876b179ba155d7362d43a0f5bdaf610
linchupeng <chuge94@163.com>
2017-01-19 00:40:26 +0800

    toolbar and navigationview

---
+++ app/src/main/java/com/lcpdev/yourweather/BaseActivity.java
@@ -0,0 +1,11 @@
+package com.lcpdev.yourweather;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by 林楚鹏 on 2017/1/17.
+ */
+
+public class BaseActivity extends AppCompatActivity {
+
+}
--- app/src/main/java/com/lcpdev/yourweather/MainActivity.java
+++ app/src/main/java/com/lcpdev/yourweather/MainActivity.java
@@ -1,13 +1,37 @@
 package com.lcpdev.yourweather;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends BaseActivity {
+    private DrawerLayout mDrawerLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        mDrawerLayout= (DrawerLayout) findViewById(R.id.drawer_layout);
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        ActionBar actionBar =getSupportActionBar();
+        if(actionBar!=null){
+            actionBar.setDisplayHomeAsUpEnabled(true);
+//            actionBar.setHomeAsUpIndicator(R.mipmap.menu);
+        }
+        navigationView.setCheckedItem(R.id.nav_city);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Toast.makeText(MainActivity.this, "点击1", Toast.LENGTH_SHORT).show();
+                return false;
+            }
+        });
     }
 }
